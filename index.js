4295daaca5810e4a191f18ae0ad4c7cf75f9096c
bamba-sigui <124349684+bamba-sigui@users.noreply.github.com>
2023-11-24 10:58:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const cors = require("cors");
 const mongoose = require("mongoose");
 const express = require("express");
-const socket = require("socket.io");
 
 const chatRoute = require("./Routes/chatRoute");
 const messageRoute = require("./Routes/messageRoute");
@@ -35,63 +34,8 @@ mongoose
   .then(() => console.log("MongoDB connection etablie..."))
   .catch((error) => console.error("MongoDB connection échouée:", error.message));
 
-server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server en marche sur le port: ${port}...`);
 });
-
-
-const io = socket(server, {
-  cors: {
-    // Put your frontend url here
-    origin: "https://realchatserver.netlify.app",
-    credentials: true,
-  },
-});
-
-let onlineUsers = [];
-
-io.on("connection", (socket) => {
-  // ajout d'utilisateur
-
-  socket.on("addNewUser", (userId) => {
-    !onlineUsers.some((user) => user.userId === userId) &&
-      onlineUsers.push({
-        userId,
-        socketId: socket.id,
-      });
-
-    console.log("Connected Users:", onlineUsers);
-
-    // send active users
-    io.emit("getUsers", onlineUsers);
-  });
-
-  // add message
-  socket.on("sendMessage", (message) => {
-    const user = onlineUsers.find(
-      (user) => user.userId === message.recipientId
-    );
-
-    if (user) {
-      console.log("sending message and notification");
-      io.to(user.socketId).emit("getMessage", message);
-      io.to(user.socketId).emit("getNotification", {
-        senderId: message.senderId,
-        isRead: false,
-        date: new Date(),
-      });
-    }
-  });
-
-  socket.on("disconnect", () => {
-    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
-    console.log("User Disconnected:", onlineUsers);
-
-    // send active users
-    io.emit("getUsers", onlineUsers);
-  });
-});
-
-
 
 
